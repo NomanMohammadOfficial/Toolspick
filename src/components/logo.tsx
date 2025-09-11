ac05424bd78f357cbb7bbcbbd2e7acc55982d387
nomanmohammadofficial <nomanmohammadofficial@gmail.com>
2025-09-11 20:33:07 +0530

    footer copyright | added logo | fixed little bugs

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -7,10 +7,9 @@ export const Logo = ({ className, uniColor }: { className?: string; uniColor?: b
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
             className={cn('text-foreground h-5 w-auto', className)}>
-            <path
-                d="M3 0H5V18H3V0ZM13 0H15V18H13V0ZM18 3V5H0V3H18ZM0 15V13H18V15H0Z"
-                fill={uniColor ? 'currentColor' : 'url(#logo-gradient)'}
-            />
+            <g transform="scale(0.375)">
+                <radialGradient id="oDvWy9qKGfkbPZViUk7TCa" cx="-670.437" cy="617.13" r=".041" gradientTransform="matrix(128.602 652.9562 653.274 -128.6646 -316906.281 517189.719)" gradientUnits="userSpaceOnUse"><stop offset="0" stopColor="#1ba1e3"/><stop offset="0" stopColor="#1ba1e3"/><stop offset=".3" stopColor="#5489d6"/><stop offset=".545" stopColor="#9b72cb"/><stop offset=".825" stopColor="#d96570"/><stop offset="1" stopColor="#f49c46"/></radialGradient><path fill={uniColor ? 'currentColor' : 'url(#oDvWy9qKGfkbPZViUk7TCa)'} d="M22.882,31.557l-1.757,4.024c-0.675,1.547-2.816,1.547-3.491,0l-1.757-4.024	c-1.564-3.581-4.378-6.432-7.888-7.99l-4.836-2.147c-1.538-0.682-1.538-2.919,0-3.602l4.685-2.08	c3.601-1.598,6.465-4.554,8.002-8.258l1.78-4.288c0.66-1.591,2.859-1.591,3.52,0l1.78,4.288c1.537,3.703,4.402,6.659,8.002,8.258	l4.685,2.08c1.538,0.682,1.538,2.919,0,3.602l-4.836,2.147C27.26,25.126,24.446,27.976,22.882,31.557z"/><radialGradient id="oDvWy9qKGfkbPZViUk7TCb" cx="-670.437" cy="617.13" r=".041" gradientTransform="matrix(128.602 652.9562 653.274 -128.6646 -316906.281 517189.719)" gradientUnits="userSpaceOnUse"><stop offset="0" stopColor="#1ba1e3"/><stop offset="0" stopColor="#1ba1e3"/><stop offset=".3" stopColor="#5489d6"/><stop offset=".545" stopColor="#9b72cb"/><stop offset=".825" stopColor="#d96570"/><stop offset="1" stopColor="#f49c46"/></radialGradient><path fill={uniColor ? 'currentColor' : 'url(#oDvWy9qKGfkbPZViUk7TCb)'} d="M39.21,44.246l-0.494,1.132	c-0.362,0.829-1.51,0.829-1.871,0l-0.494-1.132c-0.881-2.019-2.467-3.627-4.447-4.506l-1.522-0.676	c-0.823-0.366-0.823-1.562,0-1.928l1.437-0.639c2.03-0.902,3.645-2.569,4.511-4.657l0.507-1.224c0.354-0.853,1.533-0.853,1.886,0	l0.507,1.224c0.866,2.088,2.481,3.755,4.511,4.657l1.437,0.639c0.823,0.366,0.823,1.562,0,1.928l-1.522,0.676	C41.677,40.619,40.091,42.227,39.21,44.246z"/>
+            </g>
             <text
                 x="24"
                 y="14"
@@ -20,21 +19,6 @@ export const Logo = ({ className, uniColor }: { className?: string; uniColor?: b
                 fontWeight="bold">
                 Toolspick
             </text>
-            <defs>
-                <linearGradient
-                    id="logo-gradient"
-                    x1="10"
-                    y1="0"
-                    x2="10"
-                    y2="20"
-                    gradientUnits="userSpaceOnUse">
-                    <stop stopColor="#9B99FE" />
-                    <stop
-                        offset="1"
-                        stopColor="#2BC8B7"
-                    />
-                </linearGradient>
-            </defs>
         </svg>
     )
 }
@@ -48,25 +32,9 @@ export const LogoIcon = ({ className, uniColor }: { className?: string; uniColor
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
             className={cn('size-5', className)}>
-            <path
-                d="M3 0H5V18H3V0ZM13 0H15V18H13V0ZM18 3V5H0V3H18ZM0 15V13H18V15H0Z"
-                fill={uniColor ? 'currentColor' : 'url(#logo-gradient)'}
-            />
-            <defs>
-                <linearGradient
-                    id="logo-gradient"
-                    x1="10"
-                    y1="0"
-                    x2="10"
-                    y2="20"
-                    gradientUnits="userSpaceOnUse">
-                    <stop stopColor="#9B99FE" />
-                    <stop
-                        offset="1"
-                        stopColor="#2BC8B7"
-                    />
-                </linearGradient>
-            </defs>
+            <g transform="scale(0.375)">
+                <radialGradient id="oDvWy9qKGfkbPZViUk7TCa" cx="-670.437" cy="617.13" r=".041" gradientTransform="matrix(128.602 652.9562 653.274 -128.6646 -316906.281 517189.719)" gradientUnits="userSpaceOnUse"><stop offset="0" stopColor="#1ba1e3"/><stop offset="0" stopColor="#1ba1e3"/><stop offset=".3" stopColor="#5489d6"/><stop offset=".545" stopColor="#9b72cb"/><stop offset=".825" stopColor="#d96570"/><stop offset="1" stopColor="#f49c46"/></radialGradient><path fill={uniColor ? 'currentColor' : 'url(#oDvWy9qKGfkbPZViUk7TCa)'} d="M22.882,31.557l-1.757,4.024c-0.675,1.547-2.816,1.547-3.491,0l-1.757-4.024	c-1.564-3.581-4.378-6.432-7.888-7.99l-4.836-2.147c-1.538-0.682-1.538-2.919,0-3.602l4.685-2.08	c3.601-1.598,6.465-4.554,8.002-8.258l1.78-4.288c0.66-1.591,2.859-1.591,3.52,0l1.78,4.288c1.537,3.703,4.402,6.659,8.002,8.258	l4.685,2.08c1.538,0.682,1.538,2.919,0,3.602l-4.836,2.147C27.26,25.126,24.446,27.976,22.882,31.557z"/><radialGradient id="oDvWy9qKGfkbPZViUk7TCb" cx="-670.437" cy="617.13" r=".041" gradientTransform="matrix(128.602 652.9562 653.274 -128.6646 -316906.281 517189.719)" gradientUnits="userSpaceOnUse"><stop offset="0" stopColor="#1ba1e3"/><stop offset="0" stopColor="#1ba1e3"/><stop offset=".3" stopColor="#5489d6"/><stop offset=".545" stopColor="#9b72cb"/><stop offset=".825" stopColor="#d96570"/><stop offset="1" stopColor="#f49c46"/></radialGradient><path fill={uniColor ? 'currentColor' : 'url(#oDvWy9qKGfkbPZViUk7TCb)'} d="M39.21,44.246l-0.494,1.132	c-0.362,0.829-1.51,0.829-1.871,0l-0.494-1.132c-0.881-2.019-2.467-3.627-4.447-4.506l-1.522-0.676	c-0.823-0.366-0.823-1.562,0-1.928l1.437-0.639c2.03-0.902,3.645-2.569,4.511-4.657l0.507-1.224c0.354-0.853,1.533-0.853,1.886,0	l0.507,1.224c0.866,2.088,2.481,3.755,4.511,4.657l1.437,0.639c0.823,0.366,0.823,1.562,0,1.928l-1.522,0.676	C41.677,40.619,40.091,42.227,39.21,44.246z"/>
+            </g>
         </svg>
     )
 }
